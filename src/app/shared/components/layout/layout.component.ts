72caf64c824d8f0468e298b1ed83672901edd1bc
Feruza Makhmudova <latipova.feruza12@gmail.com>
2023-12-15 17:41:46 +0500

    fix: email display; still hate it

--- src/app/shared/components/layout/layout.component.ts
+++ src/app/shared/components/layout/layout.component.ts
@@ -5,12 +5,14 @@ import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
 @Component({
   selector: 'app-layout',
   standalone: true,
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.sass'],
-  imports: [RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule, NzButtonModule],
+  imports: [RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule, NzButtonModule, CommonModule, NzTypographyModule],
 })
 export class LayoutComponent implements OnInit {
   isCollapsed = false;
